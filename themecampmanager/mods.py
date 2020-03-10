8eb988ab24bc98e3f4facaa50766d852c55be9de
Your name hereChristopher <christopher.filkins@gmail.com>
2020-03-10 13:24:11 -0700

    Added User Class

--- themecampmanager/mods.py
+++ themecampmanager/mods.py
@@ -7,8 +7,10 @@ def return_hash():
     '''
     Generates a random string of alphanumeric characters of length 18
     '''
-    hash_id = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(18))
+    hash_id = ''.join(secrets.choice(
+        string.ascii_letters + string.digits) for i in range(18))
     output_hash(hash_id)
+
 
 def output_hash(hash_id):
     '''
@@ -16,12 +18,19 @@ def output_hash(hash_id):
     '''
     print(hash_id)
 
+
 def retrieve_hashes():
     '''
     Currently this is a static list but this will be a pipeline to retrieve
     hashes from the db of members to check for matches
     '''
-    hashes = ['0xhO48RWRBv7CBaCt3','E4KaCetWk6yP6jy9E6','6o0IX5x3ToyI6Pqh5f']
+    hashes = ['0xhO48RWRBv7CBaCt3', 'E4KaCetWk6yP6jy9E6', 
+              '6o0IX5x3ToyI6Pqh5f']
+
+def import_sheet(url):
+    df = pd.read_csv(url)
+    print(df)
+
 
 def check_hash(hash_id):
     '''
@@ -30,3 +39,16 @@ def check_hash(hash_id):
     '''
     pass
 
+class User:
+    '''
+    Base camp membership class
+    '''
+    def __init__(self, fname, lname, email, id_hash, alpha_hash):
+        self.fname = fname #first name
+        self.lname = lname #last name
+        self.email = email #checked against profile
+        self.id_hash = id_hash #unique identifier
+        self.alpha_hash = alpha_hash #concat name for url
+
+    def fullname(self):
+        return '{} {}'.format(self.fname, self.lname)
--- themecampmanager/scr.py
+++ themecampmanager/scr.py
@@ -1,3 +1,11 @@
 from themecampmanager.mods import return_hash
+import pandas as pd
 
-return_hash()
+#return_hash()
+
+
+url = "2020SC.csv"
+#import_sheet(url)
+df = pd.read_csv(url)
+print(df['Firstname'][0], df['Surname'][0], df['E-mail'][0], 
+      df['ID-Hash'][0], df['ID-Hash-Lower'][0])
